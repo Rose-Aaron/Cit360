2b4b2ecb31c0a6b1f49e84ea61e4f244bce82819
Rose-Aaron <skyline4911@gmail.com>
2017-05-09 11:28:38 -0600

    Update AppControl.java

--- AppPatternControl/AppControl/AppControl.java
+++ AppPatternControl/AppControl/AppControl.java
@@ -7,7 +7,7 @@ public class AppControl
   public static void main( String[] args )
   {
     Scanner input = new Scanner(System.in);
-    System.out.println("Please type Episode 1, Episode 2, or Episode 3 to go to a page.");
+    System.out.println("Please type EpisodeOne, EpisodeTwo, or EpisodeThree to go to a page.");
     AppController app = new AppController();
     app.dispatchRequest(input.next());
       
