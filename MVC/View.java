2d07ef0243fd2c48e3a510412d171abb3d3655c7
Rose-Aaron <skyline4911@gmail.com>
2017-05-14 11:46:05 -0600

    Create View.java

---
+++ MVC/View.java
@@ -0,0 +1,9 @@
+package mvc;
+
+public class ModelView {
+   public void printModelDetails(String modelName, String modelRollNo){
+      System.out.println("Model: ");
+      System.out.println("Name: " + modelName);
+      System.out.println("Roll No: " + modelRollNo);
+   }
+}
