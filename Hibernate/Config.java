034a0518a5a58eac0e424c79c7babc0bb80a12e3
Rose-Aaron <skyline4911@gmail.com>
2017-05-31 13:28:46 -0600

    Update Config.java

--- Hibernate/Config.java
+++ Hibernate/Config.java
@@ -17,26 +17,19 @@ public class HibernateConfig
 
         config.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:7071/test");
 
-        //change the next two lines of code to match your MySQL user name and password.
+        config.setProperty("hibernate.connection.username", "aaron");
 
-        config.setProperty("hibernate.connection.username", "bob");
-
-        config.setProperty("hibernate.connection.password", "somepass");
+        config.setProperty("hibernate.connection.password", "skline");
 
         //change the pool size to reflect how many users you expect your application to have initially
 
-        config.setProperty("hibernate.connection.pool_size", "10");
+        config.setProperty("hibernate.connection.pool_size", "4");
 
         config.setProperty("hibernate.connection.autocommit", "true");
+	    
         config.setProperty("hibernate.id.new_generator_mappings", "false");
 
         config.setProperty("hibernate.cache.provider_class", "org.hibernate.cache.NoCacheProvider");
-
-		/*
-		 * un-comment the next line of code if you want to be able to drop and recreate tables for your data classes listed below.  This is generally a bad idea for security reasons.
-		 */
-
-        //config.setProperty("hibernate.hbm2ddl.auto", "create-drop");
 
         config.setProperty("hibernate.show_sql", "true");
 
@@ -50,6 +43,8 @@ public class HibernateConfig
 		 */
 
         config.addAnnotatedClass(User.class);
+	
+	config.addAnnotatedClass(User.class);
 
         config.addAnnotatedClass(PhoneNumber.class);
 /*
