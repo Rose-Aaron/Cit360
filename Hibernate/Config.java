9adfca38b3ce8c785900b8194585b0a92a7df5a6
Rose-Aaron <skyline4911@gmail.com>
2017-05-31 13:35:14 -0600

    Update Config.java

--- Hibernate/Config.java
+++ Hibernate/Config.java
@@ -13,15 +13,11 @@ public class HibernateConfig
 
         config.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
 
-        //change the next line of code to match your MySQL URL and port
-
-        config.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:7071/test");
+        config.setProperty("hibernate.connection.url", "adr:mysql://localhost:0911");
 
         config.setProperty("hibernate.connection.username", "aaron");
 
         config.setProperty("hibernate.connection.password", "skline");
-
-        //change the pool size to reflect how many users you expect your application to have initially
 
         config.setProperty("hibernate.connection.pool_size", "4");
 
@@ -37,22 +33,15 @@ public class HibernateConfig
 
         config.setProperty("hibernate.current_session_context_class", "thread");
 
-		/*
-		 *  Add your classes here that you want to match your database tables
-		 *  The example has a User and a PhoneNumber class.
-		 */
-
-        config.addAnnotatedClass(User.class);
+        config.addAnnotatedClass(Employee.class);
 	
-	config.addAnnotatedClass(User.class);
-
-        config.addAnnotatedClass(PhoneNumber.class);
-/*
- * There have been several changes to the Hibernate libraries.
- * Please uncomment the source code for the version of Hibernate you are using.
-*/
-         /*Hibernate 4.3 - 5.x */ //ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
-        /*Hibernate 3.x - 4.2*/ ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(config.getProperties()).buildServiceRegistry();
+	config.addAnnotatedClass(Department.class);
+	    
+	config.addAnnotatedClass(Hibernate.class);
+	    
+	config.addAnnotatedClass(Main.class);
+	    
+ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(config.getProperties()).buildServiceRegistry();
 
         sessionFactory = config.buildSessionFactory(serviceRegistry);
     }
