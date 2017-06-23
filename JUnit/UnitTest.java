bcccfeb83edea8a26e4cbc9e95393f0610a9b2ab
Rose-Aaron <skyline4911@gmail.com>
2017-06-23 11:53:41 -0600

    Update UnitTest.java

--- JUnit/UnitTest.java
+++ JUnit/UnitTest.java
@@ -9,3 +9,29 @@ public class UnitTest {
         assertFalse (Unit.getTheBoolean());
     }
 }
+
+public class UnitTest {
+
+    @Test
+    public void testGetTheObject() {
+        Unit Unit = new Unit();
+
+        assertNull (Unit.getTheObject());
+
+        assertNotNull (Unit.getTheObject());
+    }
+}
+
+public class UnitTest {
+
+    @Test
+    public void testGetTheSameObject() {
+        Unit Unit = new Unit();
+
+        assertSame   (Unit.getTheSameObject(),
+                      Unit.getTheSameObject());
+        
+        assertNotSame (Unit.getTheSameObject(),
+                      Unit.getTheSameObject());
+    }
+}
